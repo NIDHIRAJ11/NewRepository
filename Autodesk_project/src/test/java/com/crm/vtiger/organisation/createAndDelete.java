c366ac936e3f3c5b5d3982109e08e3410df90f38
Admin <Admin@DESKTOP-SJK8D8N>
2021-05-09 15:25:50 +0530

    new commit with crete dete class file

---
+++ Autodesk_project/src/test/java/com/crm/vtiger/organisation/createAndDelete.java
@@ -0,0 +1,13 @@
+package com.crm.vtiger.organisation;
+
+import org.testng.annotations.Test;
+
+public class createAndDelete 
+{
+	@Test
+	public void run()
+	{
+		System.out.println("newly created file");
+	}
+
+}
